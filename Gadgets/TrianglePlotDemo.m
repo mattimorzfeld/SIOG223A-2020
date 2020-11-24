5aff776b4dcf1f4d8270ab27b0db6258c333557b
Matti Morzfeld <19172735+mattimorzfeld@users.noreply.github.com>
2020-11-24 12:49:25 -0800

    Add files via upload

---
+++ Gadgets/TrianglePlotDemo.m
@@ -0,0 +1,25 @@
+%%
+clear
+close all
+clc
+
+
+%% Make a 6-dimensional Gaussian and draw a larger number of samples
+n = 6;
+C = 2*diag(ones(n,1))-diag(ones(n-1,1),-1)-diag(ones(n-1,1),1);
+sqrtC = sqrtm(C);
+nos = 1e5;
+X = zeros(n,nos);
+for kk=1:nos
+    X(:,kk)=sqrtC*randn(n,1);
+end
+
+
+%% Plot the samples using the triangle plotter
+%% The second argument controls the number of bins in your 2D histograms.
+%% 1 is the default number, use values less than 1 if you have only a few samples
+figure
+TrianglePlot(X,.1)
+
+figure
+TrianglePlot(X,1)
